b16eb3efa6e122e06f94a825b9dfcd4e0bebcbf7
provolot <provolot@gmail.com>
2013-10-19 17:25:10 -0400

    masking working

--- shapemaker.js
+++ shapemaker.js
@@ -18,10 +18,11 @@ var events = require("events"),
 // for test purposes:
 // http://vps.provolot.com/site2site/pinpoint/testphoto1.JPG
 // 638 x 479
-var example_date = 
+
+var example_data_1 = 
 {
-	image_width: 638,
-	image_height: 479,
+	image_width: 816,
+	image_height: 612,
 	coordinates: [
 		[50, 50],
 		[500, 60],
@@ -31,26 +32,38 @@ var example_date =
 		[300, 300],
 	]
 }
-/* var groupPath = function(d) {
-        return "M" + d3.geom.hull(d.values.map(function(i) {
-            console.log(i);
-            return [$(i).attr("cx"), $(i).attr("cy")];
-        })).join("L") + "Z";
-    }; */
+
+var example_data_2 = 
+{
+	image_width: 816,
+	image_height: 612,
+	coordinates: [
+		[282,84],
+		[468,300],
+		[298,374],
+		[80,278],
+	]
+}
+
+var data = example_data_2;
 
 //get convex hull of coordinates
-var coordhull = d3.geom.hull(example_date["coordinates"]);
-var coordhull_svg = "M" + coordhull.join("L") + "Z";
+var coordhull = d3.geom.hull(data["coordinates"]);
+var coordhull_svg = "polygon " + coordhull.join(" ") ;
+var maxX = data["image_width"];
+var maxY = data["image_height"];
+var orgimg = "images/testphoto2.jpg";
+var destimg = "polygon.png";
 
-console.log(coordhull_svg);
-console.log("yo");
-
-
+//console.log(coordhull_svg);
 //testcommand = "convert -size 213x160 xc:white -stroke black -strokewidth 1 -fill none -draw \"circle 106,80 106,10\" circle.jpg";
-imcommand = "convert -size 638x479 xc:white -stroke black -strokewidth 1 -fill none -draw \"polygon 50,50 500,60 60,300 510,310\" polygon.jpg";
+//imcommand = "convert -size 638x479 xc:white -stroke black -strokewidth 1 -fill none -draw \"polygon 50,50 500,60 60,300 510,310\" polygon.jpg";
+//imcommand = "convert -size " + maxX + "x" + maxY + " xc:none -stroke none -fill black -draw \"" + coordhull_svg + "\" " + destimg;
+imcommand = "convert -size " + maxX + "x" + maxY + " xc:black -stroke none -fill white -draw \"" + coordhull_svg + "\" -write mpr:mask +delete " + orgimg + " mpr:mask -alpha Off -compose CopyOpacity -composite " + destimg;
+
 
 this.exec_process = exec(imcommand, function (error, stdout, stderr) {
-  console.log('stdout: ' + stdout);
+//  console.log('stdout: ' + stdout);
 //  console.log('stderr: ' + stderr);
   if (error !== null) {
     console.log('exec error: ' + error);
