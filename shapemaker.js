b04db58cace0c20c8b4b0ec61a317661d5f0b394
provolot <provolot@gmail.com>
2013-10-19 18:06:16 -0400

    to use shapemaker as importable module

--- shapemaker.js
+++ shapemaker.js
@@ -67,53 +67,75 @@ var destimg = orgimg + ".shaped.png";
 //////////////// TEST DATA END
 ////////////////////////////
 
-
-//get maxX and maxY from data
-var maxX = data["image_width"];
-var maxY = data["image_height"];
-
-var mask_svg = "";
-
-if(data["coordinates"].length >= 3) {
-	//DRAW A POLYGON
-
-	//get convex hull of coordinates
-	var coordhull = d3.geom.hull(data["coordinates"]);
-	var mask_svg = "polygon " + coordhull.join(" ") ;
-} else if(data["coordinates"].length >= 2) {
-	//DRAW A CIRCLE
-
-	//circle x0,y0 x1,y1
-	//The first point x0,y0 is the center, the second point x1,y1 is any other point on the circle, this being used to calculate a radius. 
-
-	//two coordinates are the outer bounds of a circle. 
-	//so: we get a midpoint between the points and use that as the center;
-	//then we use any point as the edge.
-
-	var midX = 
-		(data["coordinates"][0][0] + 
-		data["coordinates"][1][0]) / 2;
-	var midY = 
-		(data["coordinates"][0][1] + 
-		data["coordinates"][1][1]) / 2;
-	var edgeX = data["coordinates"][0][0];
-	var edgeY = data["coordinates"][1][0];
-	
-	mask_svg = "circle " + midX + "," + midY + " " + edgeX + "," + edgeY;
+function shapemaker() {
 }
 
-//chained imagemagick command - create a mask, mask it, write to destimg
-imcommand = "convert -size " + maxX + "x" + maxY + " xc:black -stroke none -fill white -draw \"" + mask_svg + "\" -write mpr:mask +delete " + orgimg + " mpr:mask -alpha Off -compose CopyOpacity -composite " + destimg;
-
-//console.log(imcommand);
-
-this.exec_process = exec(imcommand, function (error, stdout, stderr) {
-//  console.log('stdout: ' + stdout);
-//  console.log('stderr: ' + stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
+util.inherits( shapemaker, events.EventEmitter );
 
 
+shapemaker.prototype.mask = function(data, orgimg, destimg) {
+
+	//get maxX and maxY from data
+	var maxX = data["image_width"];
+	var maxY = data["image_height"];
+
+	var mask_svg = "";
+
+	if(data["coordinates"].length >= 3) {
+		//DRAW A POLYGON
+
+		//get convex hull of coordinates
+		var coordhull = d3.geom.hull(data["coordinates"]);
+		var mask_svg = "polygon " + coordhull.join(" ") ;
+	} else if(data["coordinates"].length >= 2) {
+		//DRAW A CIRCLE
+
+		//circle x0,y0 x1,y1
+		//The first point x0,y0 is the center, the second point x1,y1 is any other point on the circle, this being used to calculate a radius. 
+
+		//two coordinates are the outer bounds of a circle. 
+		//so: we get a midpoint between the points and use that as the center;
+		//then we use any point as the edge.
+
+		var midX = 
+			(data["coordinates"][0][0] + 
+			data["coordinates"][1][0]) / 2;
+		var midY = 
+			(data["coordinates"][0][1] + 
+			data["coordinates"][1][1]) / 2;
+		var edgeX = data["coordinates"][0][0];
+		var edgeY = data["coordinates"][1][0];
+		
+		mask_svg = "circle " + midX + "," + midY + " " + edgeX + "," + edgeY;
+	}
+
+	//chained imagemagick command - create a mask, mask it, write to destimg
+	imcommand = "convert -size " + maxX + "x" + maxY + " xc:black -stroke none -fill white -draw \"" + mask_svg + "\" -write mpr:mask +delete " + orgimg + " mpr:mask -alpha Off -compose CopyOpacity -composite " + destimg;
+
+	//console.log(imcommand);
+
+	this.exec_process = exec(imcommand, function (error, stdout, stderr) {
+	//  console.log('stdout: ' + stdout);
+	//  console.log('stderr: ' + stderr);
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		}
+
+		//emit exit signal for process chaining over time
+//		this.emit( "exit", destimg);
+
+	});
+
+
+
+
+
+
+
+};
+
+
+module.exports = shapemaker;
+
+shapemaker.prototype.mask(data, orgimg, destimg);
 
